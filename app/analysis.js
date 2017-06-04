6cf0e0b0db36edffb0e32f3bb252acdab9fd82ab
paolo215 <paolo_villanueva215@yahoo.com>
2017-06-04 15:27:02 -0400

    Added more graphs

--- app/analysis.js
+++ app/analysis.js
@@ -8,6 +8,7 @@ angular.module("main").directive("analysis", function() {
         controller: ["$scope", "$http", function analysisController($scope, $http) {
 
         var barChart;
+        $scope.yes_no_ids = [];
                 
         // hard coded annual hours worth of sunshine values. 
         //I couldn't find an easy to use and easy to understand API
@@ -44,12 +45,35 @@ angular.module("main").directive("analysis", function() {
 
         $http.get("analysis.json").then(function(data) {
             data = data.data;
+            var yes_no_data = data["yes_no"];
+            for(var item in yes_no_data["questions"]) {
+                $scope.yes_no_ids.push("bar" + item.toString());
+            };
             var scale_data = data["scale"];
             var scale_locations_data = scale_data["locations"];
 
             var evaluated_scale_data = evaluate_scale(scale_locations_data);
             barChart = createBarChart(evaluated_scale_data);              
 
+
+            var hasRegistered = false;
+
+            $scope.$watch(function() {
+                if(hasRegistered) return;
+                hasRegistered = true;
+                $scope.$$postDigest(function() {
+                    hasRegistered = false;
+                    for(var item in yes_no_data["questions"]) {
+                        var question = yes_no_data["questions"][item];
+                        var question_data = yes_no_data["results"][question];
+                        var yes = question_data["yes"]["locations"];
+                        var no = question_data["no"]["locations"];
+                        var evaluate_yes_no_data = evaluate_yes_no(yes, no);
+                        var yes_no_chart = createClusteredBarChart($scope.yes_no_ids[item], question, evaluate_yes_no_data);
+                    };
+
+                });
+            });
         });
 
 
@@ -67,11 +91,29 @@ angular.module("main").directive("analysis", function() {
             return scale;
         };
 
+
+        var evaluate_yes_no = function(yes, no) {
+            var scale = [];
+            for(var item in sorted) {
+                var location = sorted[item];
+                scale.push({ 
+                    "region" : location,
+                    "yes_mean" : Number(yes[location]["mean"].toPrecision(3)),
+                    "yes_error" : Number(yes[location]["error"].toPrecision(3)),
+                    "no_mean" : Number(no[location]["mean"].toPrecision(3)),
+                    "no_error" : Number(no[location]["error"].toPrecision(3)),
+                    "sunshine": sunshine[location],
+                   });
+            };
+            return scale;
+        };
+
         var createBarChart = function(data) {
             var chart = AmCharts.makeChart("analysisBarChart", {
                 "type": "serial",
                 "dataProvider": data,
-                "startDuration": 1,
+                "startDuration": 0.2,
+                "titles": [{"text": "Average Happiness Scores", "size": 15}],
                 "valueAxes": [{
                     "id": "v1",
                     "axisAlpha": 0,
@@ -79,7 +121,8 @@ angular.module("main").directive("analysis", function() {
                     "maximum": 70,
                     "title": "Average score",
                 }],
-                "graphs": [{
+                "graphs": [
+                {
                     "balloonText" : "Value:<b>[[mean]]</b><br>Error:<b>[[error]]</b>" +
                                     "<br>Sunshine:<b>[[sunshine]] days</b>",
                     "labelText": "[[mean]]",
@@ -91,8 +134,24 @@ angular.module("main").directive("analysis", function() {
                     "valueField": "mean",
                     "bulletAxis": "v1",
                     "fillAlphas": 1,
+                    "title": "Bar",
 
-                }],
+                },
+                {
+                    "balloonText" : "Value:<b>[[mean]]</b><br>Error:<b>[[error]]</b>" +
+                                    "<br>Sunshine:<b>[[sunshine]] days</b>",
+                    "type": "smoothedLine",
+                    "lineThickness": 2,
+                    "valueField": "mean",
+                    "bulletAxis": "v2",
+                    "fillAlphas": 0,
+                    "title": "Smoothed Line",
+                    "hidden": true,
+                },
+
+
+                ],
+                "legend": { "useGraphSettings": true },
                 "categoryField": "region",
                 "categoryAxis": {
                     "gridPosition": "start",
@@ -102,12 +161,98 @@ angular.module("main").directive("analysis", function() {
                     },
                     "title" : "Region",
                 },
-                "export": {
-                    "enabled" : true,
-                },
 
             });
             return chart;
+        };
+
+        var createClusteredBarChart = function(id, question, data) {
+            var chart = AmCharts.makeChart(id, {
+                "type": "serial",
+                "dataProvider": data,
+                "startDuration": 0.2,
+                "titles": [ {"text": question, "size": 15 }],
+                "valueAxes": [{
+                    "id": "v1",
+                    "axisAlpha": 0,
+                    "minimum": 0,
+                    "maximum": 70,
+                    "title": "Average score",
+                }],
+                "graphs": [
+                {
+                    "balloonText" : "Value:<b>[[yes_mean]]</b><br>Error:<b>[[yes_error]]</b>" +
+                                    "<br>Sunshine:<b>[[sunshine]] days</b>",
+                    "labelText": "[[yes_mean]]",
+                    "type": "column",
+                    "bullet": "yError",
+                    "bulletColor": "#000",
+                    "errorField": "yes_error",
+                    "lineThickness": 2,
+                    "title": "Yes",
+                    "valueField": "yes_mean",
+                    "bulletAxis": "v1",
+                    "fillAlphas": 1,
+                    "color": "#FF0000",
+
+                },
+                {
+                    "balloonText" : "Value:<b>[[yes_mean]]</b><br>Error:<b>[[yes_error]]</b>" +
+                                    "<br>Sunshine:<b>[[sunshine]] days</b>",
+                    "type": "smoothedLine",
+                    "lineThickness": 2,
+                    "valueField": "yes_mean",
+                    "bulletAxis": "v2",
+                    "fillAlphas": 0,
+                    "title": "Yes Smoothed Line",
+                    "hidden": true,
+                },
+                {
+                    "balloonText" : "Value:<b>[[no_mean]]</b><br>Error:<b>[[no_error]]</b>" +
+                                    "<br>Sunshine:<b>[[sunshine]] days</b>",
+                    "labelText": "[[no_mean]]",
+                    "type": "column",
+                    "bullet": "yError",
+                    "bulletColor": "#000",
+                    "errorField": "no_error",
+                    "title": "No",
+                    "lineThickness": 2,
+                    "valueField": "no_mean",
+                    "bulletAxis": "v1",
+                    "fillAlphas": 1,
+                    "color": "#0000FF",
+                },
+                {
+                    "balloonText" : "Value:<b>[[no_mean]]</b><br>Error:<b>[[no_error]]</b>" +
+                                    "<br>Sunshine:<b>[[sunshine]] days</b>",
+                    "type": "smoothedLine",
+                    "lineThickness": 2,
+                    "valueField": "no_mean",
+                    "bulletAxis": "v2",
+                    "fillAlphas": 0,
+                    "title": "No Smoothed Line",
+                    "hidden": true,
+                },
+
+                ],
+                "legend": { "useGraphSettings": true },
+                "categoryField": "region",
+                "categoryAxis": {
+                    "gridPosition": "start",
+                    "axisAlpha": 0,
+                    "labelFunction": function(value) {
+                        return value + "<br>" + sunshine[value] + " days";
+                    },
+                    "title" : "Region",
+                },
+                
+                //"export": {
+                //    "enabled" : true,
+                //},
+
+            });
+            return chart;
+
         };
 
 
